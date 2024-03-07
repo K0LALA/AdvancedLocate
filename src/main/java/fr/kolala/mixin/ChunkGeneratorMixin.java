6284783a35af96031d29412a725eeebdda8bdd09
Kolala <kolala08@outlook.fr>
2024-03-07 20:09:20 +0100

    Removed StructureStartMixin since custom methods can't be used because of StructureStart being final Changed the way already found structures are implement in the conditions for return Removed client related files, since the mod is server-side

--- src/main/java/fr/kolala/mixin/ChunkGeneratorMixin.java
+++ src/main/java/fr/kolala/mixin/ChunkGeneratorMixin.java
@@ -3,7 +3,6 @@ package fr.kolala.mixin;
 import com.mojang.datafixers.util.Pair;
 import fr.kolala.util.IChunkGeneratorCustomMethods;
 import fr.kolala.util.IChunkGeneratorInvoker;
-import fr.kolala.util.IStructureStartCustomMethods;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.structure.StructureStart;
 import net.minecraft.util.math.BlockPos;
@@ -21,21 +20,12 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 
 import java.util.Set;
-import java.util.UUID;
 
 @Mixin(ChunkGenerator.class)
 public abstract class ChunkGeneratorMixin implements IChunkGeneratorInvoker, IChunkGeneratorCustomMethods {
 
-    public boolean advancedLocate$checkCommandReferenced(StructureAccessor structureAccessor, StructureStart start, UUID uuid) {
-        if (((IStructureStartCustomMethods) start).advancedLocate$isCommandReferenced(uuid)) {
-            structureAccessor.incrementReferences(start);
-            return true;
-        }
-        return false;
-    }
-
     @Override
-    public Pair<BlockPos, RegistryEntry<Structure>> advancedLocate$locateStructure(Set<RegistryEntry<Structure>> structures, WorldView world, StructureAccessor structureAccessor, boolean skipReferencedStructures, StructurePlacement placement, ChunkPos pos, Pair<Boolean, UUID> checkIfCommandReferenced) {
+    public Pair<BlockPos, RegistryEntry<Structure>> advancedLocate$locateStructure(Set<RegistryEntry<Structure>> structures, WorldView world, StructureAccessor structureAccessor, boolean skipReferencedStructures, StructurePlacement placement, ChunkPos pos, Set<Pair<BlockPos, RegistryEntry<Structure>>> alreadyFoundStructures) {
         for (RegistryEntry<Structure> registryEntry : structures) {
             StructurePresence structurePresence = structureAccessor.getStructurePresence(pos, registryEntry.value(), skipReferencedStructures);
             if (structurePresence == StructurePresence.START_NOT_PRESENT) continue;
@@ -44,16 +34,19 @@ public abstract class ChunkGeneratorMixin implements IChunkGeneratorInvoker, ICh
             }
             Chunk chunk = world.getChunk(pos.x, pos.z, ChunkStatus.STRUCTURE_STARTS);
             StructureStart structureStart = structureAccessor.getStructureStart(ChunkSectionPos.from(chunk), registryEntry.value(), chunk);
-            boolean checkingForCommandReference = checkIfCommandReferenced.getFirst();
-            UUID commandUUID = checkIfCommandReferenced.getSecond();
-            if (structureStart == null || !structureStart.hasChildren() || (skipReferencedStructures && !IChunkGeneratorInvoker.invokeCheckNotReferenced(structureAccessor, structureStart)) || (checkingForCommandReference && advancedLocate$checkCommandReferenced(structureAccessor, structureStart, commandUUID))) continue;
-            return Pair.of(placement.getLocatePos(structureStart.getPos()), registryEntry);
+
+            if (structureStart == null ||
+                !structureStart.hasChildren()) continue;
+            Pair<BlockPos, RegistryEntry<Structure>> pair = Pair.of(placement.getLocatePos(structureStart.getPos()), registryEntry);
+            if ((skipReferencedStructures && !IChunkGeneratorInvoker.invokeCheckNotReferenced(structureAccessor, structureStart))
+                || (alreadyFoundStructures != null && !alreadyFoundStructures.contains(pair))) continue;
+            return pair;
         }
         return null;
     }
 
     @Unique
     private Pair<BlockPos, RegistryEntry<Structure>> locateStructure(Set<RegistryEntry<Structure>> structures, WorldView world, StructureAccessor structureAccessor, boolean skipReferencedStructures, StructurePlacement placement, ChunkPos pos) {
-        return advancedLocate$locateStructure(structures, world, structureAccessor, skipReferencedStructures, placement, pos, new Pair<Boolean, UUID>(false, UUID.randomUUID()));
+        return advancedLocate$locateStructure(structures, world, structureAccessor, skipReferencedStructures, placement, pos, null);
     }
 }
